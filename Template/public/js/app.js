469f8b0e486f4f484805a38a7f8fdd6f75ba2db7
BETHA\bruno.kurzawe <compras@4>
2021-11-19 19:52:34 -0300

    Ajustes finais

--- Template/public/js/app.js
+++ Template/public/js/app.js
@@ -28,6 +28,69 @@ angular
             .when("/estado/new", {
                 templateUrl: "partials/estado-form.html",
             })
+            .when("/pais", {
+                templateUrl: "partials/pais.html",
+            })
+            .when("/pais/:id", {
+                templateUrl: "partials/pais-form.html",
+            })
+            .when("/pais/new", {
+                templateUrl: "partials/pais-form.html",
+            })
+            .when("/marca", {
+                templateUrl: "partials/marca.html",
+            })
+            .when("/marca/:id", {
+                templateUrl: "partials/marca-form.html",
+            })
+            .when("/marca/new", {
+                templateUrl: "partials/marca-form.html",
+            })
+            .when("/modelo", {
+                templateUrl: "partials/modelo.html",
+            })
+            .when("/modelo/:id", {
+                templateUrl: "partials/modelo-form.html",
+            })
+            .when("/modelo/new", {
+                templateUrl: "partials/modelo-form.html",
+            })
+            .when("/opcional", {
+                templateUrl: "partials/opcional.html",
+            })
+            .when("/opcional/:id", {
+                templateUrl: "partials/opcional-form.html",
+            })
+            .when("/opcional/new", {
+                templateUrl: "partials/opcional-form.html",
+            })
+            .when("/cliente", {
+                templateUrl: "partials/cliente.html",
+            })
+            .when("/cliente/:id", {
+                templateUrl: "partials/cliente-form.html",
+            })
+            .when("/cliente/new", {
+                templateUrl: "partials/cliente-form.html",
+            })
+            .when("/veiculo", {
+                templateUrl: "partials/veiculo.html",
+            })
+            .when("/veiculo/:id", {
+                templateUrl: "partials/veiculo-form.html",
+            })
+            .when("/veiculo/new", {
+                templateUrl: "partials/veiculo-form.html",
+            })
+            .when("/locacao", {
+                templateUrl: "partials/locacao.html",
+            })
+            .when("/locacao/:id", {
+                templateUrl: "partials/locacao-form.html",
+            })
+            .when("/locacao/new", {
+                templateUrl: "partials/locacao-form.html",
+            })
             .otherwise({
                 templateUrl: "partials/404.html",
             });
