09ce5e97672ae9c0e43e04a7b164f55c53e0c970
BETHA\bruno.kurzawe <compras@4>
2021-11-20 10:14:45 -0300

    Ajustes finais

--- Template/public/js/controllers/cidades.controller-form.js
+++ Template/public/js/controllers/cidades.controller-form.js
@@ -38,14 +38,47 @@
         function salvar() {
             CidadeService.save(vm.cidade).success(function () {
                 $location.path("/cidade");
-                alert("Cidade cadastrada com sucesso!!");
+                //alert("Cidade cadastrada com sucesso!!");
+                message('sucesso','Cidade cadastrada com sucesso!!');
             }).error(function (erro) {
-                alert(JSON.stringify(erro));
+                //alert(JSON.stringify(erro));
+                let msg = erro;
+                if(msg.erro){
+                    message('erro',msg.erro);
+                }else{
+                    message('erro',JSON.stringify(erro));
+                }
+                
             });
         }
 
     function select(valor){
         return '"'+valor+'"';
+    }
+
+    function message(tipo, mensagem) {
+        let text = "";
+        if (tipo === 'info') {
+            tipo = 'alert alert-info';
+            text = 'Informação!';
+        }
+        if (tipo === 'sucesso') {
+            tipo = 'alert alert-success';
+            text = 'Sucesso!';
+        }
+        if (tipo === 'erro') {
+            tipo = 'alert alert-danger';
+            text = 'Erro!';
+        }
+        let message = '<div id="alerta" class="' + tipo + '" id="bsalert">';
+        message += '    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> ';
+        message += '    <strong>' + text + '</strong> ' + mensagem + '  ';
+        message += ' </div> ';
+        $("#divPrincipal").append(message);
+
+        setTimeout(function(){ 
+            $("#alerta").alert('close');
+         }, 3000);
     }
 
     }
